5455c81d60c06cdb4697fc7fd58974bdb102ca06
Michal Forman <miska.forman@gmail.com>
2024-02-09 22:57:30 +0100

    hopefully already fixed everything

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-import App from "./mainApp";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
